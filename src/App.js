ec212220f563dc590b16d779984c44d92e7b0472
Matthew Francis <mattfrancis888@gmail.com>
2019-03-02 22:15:09 -0500

    Moved import for custom css file so that reactstrap won't override it

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import Header from './components/Header.js'
 import Body from './components/Body.js'
 import Footer from './components/Footer.js'
 import Testme from './components/Testme.js'
-import './css/main.css'
 
 class App extends Component {
   render() {
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/main.css'
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
