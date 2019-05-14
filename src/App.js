962a68de94f9e13f9627d0677a413db3381dd0b5
mattfrancis888 <mattfrancis888@gmail.com>
2019-05-14 16:44:52 -0400

    "Sign in" pop up box layout is responsive for all screens.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
 import {BrowserRouter,Route} from 'react-router-dom';
+
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faStar } from '@fortawesome/free-solid-svg-icons';
+import { faStar, faTimes } from '@fortawesome/free-solid-svg-icons';
+
 import Header from './components/Header.js';
 import Body from './components/Body.js';
 import Footer from './components/Footer.js';
@@ -14,20 +16,31 @@ import Buy_Item from './components/Buy_Item.js';
 import Buy_Item_Info from './components/Buy_Item_Info.js';
 import Sign_In_Box from './components/Sign_In_Box.js';
 
-library.add(faStar);
+library.add(faStar, faTimes);
+
 class App extends Component {
+  state = {
+      hideHeaderAndFooter: false
+  }
+  myCallback = (booleanFromChild) => {
+    {/*If sign in button is clicked, <Header> and <Footer> should dissapear */}
+    this.setState({
+      hideHeaderAndFooter: booleanFromChild
+    });
+    console.log(booleanFromChild);
+  }
+
   render() {
     return (
       <BrowserRouter>
         <div className="App">
-          <Header/>
+          <Header callbackFromParent={this.myCallback}/>
+
           <Route exact path="/" render = {() => <Featured/> } />
           {/*Passing component Item_Info, as a children is created for practice */}
           <Route path="/items-available" render = {() => <Items_Available item_info={<Item_Info/>}/> } />
           <Route path="/test" render = {() => <Sign_In_Box/> } />
           <Route path="/buy-item" render = {() => <Buy_Item buy_item_info={<Buy_Item_Info/>}
-
-
             item_info={<Item_Info/>}/> } />
           <Footer/>
         </div>
--- src/components/Sign_In_Box.js
+++ src/components/Sign_In_Box.js
@@ -3,21 +3,27 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 const Sign_In_Box = (props) => {
   return(
     <>
-    <form className="p-5">
+    <div className="popup-out">
+      <div className="popup-in">
+        <form className="p-5">
+          <span className="d-flex justify-content-end">
+            <FontAwesomeIcon icon="times" className="fa-lg" style={ {color: "#767676"} }/>
+          </span>
+          <h1 className="text-center" id="sign-in-box-title"> Sign in </h1>
 
-      <h1 className="text-center" id="sign-in-box-title"> Sign in </h1>
+          <div className="form-group mb-4 w-100">
+            <label for="exampleInputEmail1" className="sign-in-labels">Email address</label>
+            <input type="email" className="form-control sign-in-input p-4" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"/>
+          </div>
 
-      <div className="form-group mb-4">
-        <label for="exampleInputEmail1" className="sign-in-labels">Email address</label>
-        <input type="email" className="form-control sign-in-input p-4" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"/>
+          <div className="form-group mb-4 w-100">
+            <label for="exampleInputPassword1">Password</label>
+            <input type="password" className="form-control sign-in-input p-4" id="exampleInputPassword1" placeholder="Password"/>
+          </div>
+          <button type="button" className="btn btn-primary btn-lg w-100 p-3" id="sign-in-button">Sign in</button>
+        </form>
       </div>
-
-      <div className="form-group mb-4">
-        <label for="exampleInputPassword1">Password</label>
-        <input type="password" className="form-control sign-in-input p-4" id="exampleInputPassword1" placeholder="Password"/>
-      </div>
-      <button type="button" className="btn btn-primary btn-lg w-100 p-3" id="sign-in-button">Sign in</button>
-    </form>
+    </div>
     </>
   );
 }
